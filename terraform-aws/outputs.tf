67e218fd854861680150e98f697460ecb2859454
tdsacilowski <tdsacilowski@gmail.com>
2018-06-12 15:30:07 -0400

    Output RDS instance info

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,8 +1,11 @@
-output "ip-addresses" {
+output "endpoints" {
   value = <<EOF
 
+Vault Demo IP (public):  ${aws_instance.vault-demo.public_ip}
+Vault Demo IP (private): ${aws_instance.vault-demo.private_ip}
 
-Vault Demo (public): ${aws_instance.vault-demo.public_ip}
-Vault Demo (private): ${aws_instance.vault-demo.private_ip}
+RDS Instance Endpoint:   ${aws_db_instance.default.endpoint}
+RDS Master Username:     ${aws_db_instance.default.username}
+RDS Master Password:     ${aws_db_instance.default.password}
 EOF
 }
